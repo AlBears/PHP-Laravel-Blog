21d73817e066c55c55aa08145b6fd00a694f100d
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-21 12:42:18 +0300

    frontend structure

---
+++ resources/views/frontend/other/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+  About me
+@endsection
+
+@section('content')
+  <h1>My name is ...</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste dolore reprehenderit quaerat, maxime necessitatibus nam at incidunt, minima iure nobis magnam explicabo esse saepe. Repudiandae accusantium molestiae pariatur ad dignissimos?</p>
+@endsection
