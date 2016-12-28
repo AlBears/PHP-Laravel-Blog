4b76609fb98e6de4016e6c974aa5807b78ee3602
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-28 09:01:15 +0300

    create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
 
 class PostController extends Controller
 {
@@ -14,5 +16,28 @@ class PostController extends Controller
     public function getSinglePost($post_id, $end = 'frontend')
     {
       return view($end .'.blog.single');
+    }
+
+    public function getCreatePost()
+    {
+      return view('admin.blog.create_post');
+    }
+
+    public function postCreatePost(Request $request)
+    {
+      $this->validate($request, [
+        'title' => 'required|max:120|unique:posts',
+        'author' => 'required|max:80',
+        'body' => 'required'
+      ]);
+
+      $post = new Post();
+      $post->title = $request['title'];
+      $post->author = $request['author'];
+      $post->body = $request['body'];
+      $post->save();
+      //attaching categories
+
+      return redirect()->route('admin.index')->with(['success' => 'Post successfully created!']);
     }
 }
--- resources/views/admin/blog/create_post.blade.php
+++ resources/views/admin/blog/create_post.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin-master')
 
 @section('styles')
-  <link rel="stylesheet" type="text/css" href="{{ URL::secure('css/form.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ URL::to('css/form.css') }}">
 @endsection
 
 @section('content')
@@ -11,12 +11,12 @@
 
       <div class="input-group">
         <label for="title">Title</label>
-        <input type="text" name="title" id="title">
+        <input type="text" name="title" id="title" {{ $errors->has('title') ? 'class=has-error' : '' }} value="{{ Request::old('title') }}">
       </div>
 
       <div class="input-group">
         <label for="author">Author</label>
-        <input type="text" name="author" id="author">
+        <input type="text" name="author" id="author" {{ $errors->has('author') ? 'class=has-error' : '' }} value="{{ Request::old('author') }}" />
       </div>
 
       <div class="input-group">
@@ -34,7 +34,7 @@
 
       <div class="input-group">
         <label for="body">Body</label>
-        <textarea name="body" id="body" rows="12"></textarea>
+        <textarea name="body" id="body" rows="12" {{ $errors->has('body') ? 'class=has-error' : '' }}>{{ Request::old('body') }}</textarea>
       </div>
 
       <button type="submit" class="btn">Create Post</button>
