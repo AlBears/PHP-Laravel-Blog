576a62c0f4ab7eff832c5bd9561d43c338dda8f2
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-12 12:19:50 +0300

    listeners and events

--- resources/views/frontend/other/contact.blade.php
+++ resources/views/frontend/other/contact.blade.php
@@ -14,22 +14,22 @@
 
     <div class="input-group">
       <label for="name">Your Name</label>
-      <input type="text" name="name" id="name">
+      <input type="text" name="name" id="name" value="{{ Request::old('name') }}">
     </div>
 
     <div class="input-group">
       <label for="email">Your E-Mail</label>
-      <input type="text" name="email" id="email">
+      <input type="text" name="email" id="email" value="{{ Request::old('email') }}">
     </div>
 
     <div class="input-group">
       <label for="subject">Subject</label>
-      <input type="text" name="subject" id="subject">
+      <input type="text" name="subject" id="subject" value="{{ Request::old('subject') }}">
     </div>
 
     <div class="input-group">
       <label for="message">Your Message</label>
-      <textarea name="message" id="message" rows="10"></textarea>
+      <textarea name="message" id="message" rows="10">{{ Request::old('message') }}</textarea>
     </div>
 
     <button type="submit" class="btn">Submit Message</button>
